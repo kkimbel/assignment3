2a73152c54c8b61ce6df2253706abc7eab845ced
kkimbel <38014742+kkimbel@users.noreply.github.com>
2018-04-02 14:29:08 -0400

    Add files via upload

---
+++ part1.s
@@ -0,0 +1,20 @@
+.section .data
+	path:
+	.ascii "/bin/sh"
+.section .text
+.globl _start
+_start:
+	leaq path, %rdi
+	xor %rsi, %rsi
+	push %rsi
+	push %rdi
+	mov %rsp, %rsi
+	xor %rdx, %rdx
+	mov $59, %rax
+	syscall
+
+	#exit system call
+	mov $0, %rdi
+	mov $60, %rax
+	syscall
+     
